32b4d81df488afff1c3f01b1dae6a621b4a793fb
An Doan <minh.an.doan@gmail.com>
2021-07-31 15:16:16 -0700

    Refactor client.js

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -2,32 +2,45 @@ let store = {
     user: { name: "Student" },
     currentRoverData: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+};
+
+// ------------------------------------------------------  COMPONENTS
 
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
 const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
+    store = Object.assign(store, newState);
+    // whenever store has new rover, update photos
     fetchPhotos(store.currentRoverData.name, store.currentRoverData.max_date);
-}
+};
+
+// HIGHER ORDER FUNCTION 1
+// render HTML elements (rendered from an array) onto a parent HTML element
+const renderElement = (element, renderFunc, arr) => {
+    element.innerHTML = '';
+    element.append(...renderFunc(arr));
+};
 
 const render = async (root, state) => {
-    root.innerHTML = App(state)
-    document.getElementsByTagName('nav')[0].append(...renderButtons(state.rovers));
-    addRoverListeners();
-}
+    root.innerHTML = App();
+    renderElement(document.getElementsByTagName('nav')[0], renderButtons, state.rovers);
+    addRoverListeners((button) => {
+        document.querySelector('.selected').classList.remove('selected');
+        button.classList.add('selected');
+        fetchManifest(button.innerText, store);
+    });
+};
 
 // add button listeners to the rovers 
-const addRoverListeners = () => {
+// HIGHER ORDER FUNCTION 2
+const addRoverListeners = (clickEventHandler) => {
     document.querySelectorAll('button').forEach(button => {
         button.addEventListener('click', function () {
-            document.querySelector('.selected').classList.remove('selected');
-            button.classList.add('selected');
-            fetchManifest(button.innerText, store);
+            clickEventHandler(button);
         });
     });
-}
+};
 
 const renderPhotos = (photos) => {
     const photoGroups = photos.map((photo, idx) => {
@@ -43,7 +56,7 @@ const renderPhotos = (photos) => {
         return photoGroup;
     });
     return photoGroups;
-}
+};
 
 const renderStats = (rover) => {
     document.getElementById('stats').innerHTML = `
@@ -51,7 +64,7 @@ const renderStats = (rover) => {
         <div><span class="key">Launch Date: </span><span class="val">${new Date(rover.get('launch_date')).toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric'})}</span></div>
         <div><span class="key">Landing Date: </span><span class="val">${new Date(rover.get('landing_date')).toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric'})}</span></div>
         `;
-}
+};
 
 const renderButtons = (buttonNames) => {
     buttons = buttonNames.map((name, idx) => {
@@ -64,13 +77,10 @@ const renderButtons = (buttonNames) => {
         return button;
     });
     return buttons;
-}
-
+};
 
 // create content
-const App = (state) => {
-    let { rovers, apod } = state
-
+const App = () => {
     return `<header>
                 <h1>Mars Rover Dashboard</h1>
                 <p>Click on a Mars Rover name to view its information and photos.</p>
@@ -81,15 +91,13 @@ const App = (state) => {
             </div>
             <div id="photos">
             </div>
-            <footer>Rover Information Taken From <a href="https://www.nasa.gov">NASA</a></footer>`
-}
+            <footer>Rover Information Taken From <a href="https://www.nasa.gov">NASA</a></footer>`;
+};
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    render(root, store)
-})
-
-// ------------------------------------------------------  COMPONENTS
+    render(root, store);
+});
 
 // ------------------------------------------------------  API CALLS
 
@@ -100,15 +108,12 @@ const fetchManifest= (name, store) => {
             renderStats(Immutable.Map(rover));
             updateStore(store, {currentRoverData: rover});
         });
-}
+};
 
-const fetchPhotos = (name, date, store) => {
-    console.log(name);
-    console.log(date);
+const fetchPhotos = (name, date) => {
     fetch(`/rover/${name}/${date}`)
         .then(res => res.json())
         .then(photos => {
-            document.getElementById('photos').innerHTML = '';
-            document.getElementById('photos').append(...renderPhotos(photos));
+            renderElement(document.getElementById('photos'), renderPhotos, photos);
         });
 }
